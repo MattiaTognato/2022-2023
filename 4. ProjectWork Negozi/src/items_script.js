eaeccf7bbc6e6aedd3761ed2e0d9e334e4e9ba82
Matwy <mattiabasket2004@gmail.com>
2022-12-12 18:26:26 +0100

    aggiunta cartella mancante

---
+++ 4. ProjectWork Negozi/src/items_script.js
@@ -0,0 +1,25 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const category = urlParams.get('category');
+const shopName= urlParams.get('shop');
+fetch('https://dummyjson.com/products/category/' + category)
+    .then((response) => response.json())
+    .then((data) => {
+        $(document).ready( function () {
+            document.getElementById("title").appendChild(document.createTextNode((" " + shopName).toUpperCase()));
+            document.title+= " " + shopName;
+            $('#table_id').DataTable({
+                data: data.products,
+                columnDefs: [{
+                    "defaultContent": "-",
+                    "targets": "_all"
+                  }],
+                columns: [
+                    {data: 'title'},
+                    {data: 'brand'},
+                    {data: 'stock'},
+                    {data: 'price'}
+                ]
+            });
+        } );
+    });
